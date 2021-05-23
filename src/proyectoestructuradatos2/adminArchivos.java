8180b51c19b531423b46925914e34810d2c104f1
aisac712 <isaac.diaz712@gmail.com>
2021-05-22 22:42:49 -0600

    Cambios en Archivo

---
+++ src/proyectoestructuradatos2/adminArchivos.java
@@ -0,0 +1,17 @@
+package proyectoestructuradatos2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class adminArchivos {
+    //private ArrayList<Archivo> listaCorreos = new ArrayList();
+    //private File archivo = null;
+    private String direccionArchivo;        //"./Correos/"+cuenta_ingresada.getUser()+"R.pr" o ".txt"
+    
+    public void cargarArchivo() throws IOException {
+        
+    }
+}
